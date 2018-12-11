904b237b93e7f0363079a31ecaf2e319aaab6ea2
saniag081 <sgonzalezhernandez081@gmail.com>
2018-12-10 22:17:01 -0500

    methods y evento click

--- js/index.js
+++ js/index.js
@@ -1,12 +1,32 @@
 const app = new Vue({
     el: '#app',
-    data:{
+    data: {
         titulo: 'Hola Mundo con vue',
         frutas: [
-            {nombre: 'fresa', cantidad: 10},
-            { nombre: 'mago', cantidad: 5},
-            { nombre: 'pera', cantidad: 2},
-            { nombre: 'manzana', cantidad: 0}
-        ]
+            { nombre: 'fresa', cantidad: 10 },
+            { nombre: 'mago', cantidad: 5 },
+            { nombre: 'pera', cantidad: 2 },
+            { nombre: 'manzana', cantidad: 0 }
+        ],
+        nuevaFruta: '',
+        total: 0
+    },
+    methods: {
+        agregarFrutas() {
+            this.frutas.push({
+                nombre: this.nuevaFruta, cantidad: 0
+            })
+            this.nuevaFruta = '';
+        }
+    },
+    
+    computed:{
+        sumarFrutas(){
+            this.total = 0;
+            for(fruta of this.frutas){
+                this.total += this.total + fruta.cantidad;
+            }
+            return this.total;
+        }
     }
 })
